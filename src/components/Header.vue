e8b62085c1f23fcf597def00e095379954b1085f
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-05 09:12:06 +0100

    wip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,9 @@
         </div>
         <div class="flex items-center">
           <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
+            <a href="#" class="py-2 px-6 flex">
+              Login
+            </a>
           </nav>
           <button class="lg:hidden flex flex-col ml-4">
                     <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
