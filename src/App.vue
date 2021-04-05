8c7dbfdcc36a375d98418b9602900513aeaeb1da
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-05 09:06:43 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <Header />
   <HelloWorld msg="Welcome to Fundz App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header";
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld
   }
 }
@@ -18,7 +21,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
