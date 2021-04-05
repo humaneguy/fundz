df22f3b921ab72acf0af407bea903e81fe9240d0
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-05 08:23:23 +0100

    wip

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div class="bg-gray-50">
+      <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+          <span class="text-indigo-600">{{ msg }}</span>
+        </h2>        <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
+        <div class="inline-flex rounded-md shadow">
+          <a href="https://medium.com/@horaceh/how-to-setup-a-tailwind-css-tailwind-ui-project-with-vue-cli-fdc671d69e48" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+            How to
+          </a>
+        </div>          <div class="ml-3 inline-flex rounded-md shadow">
+        <a href="https://github.com/horaceho/vue-tailwind-skeleton" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+          Github
+        </a>
+      </div>
+      </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,19 +30,3 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
