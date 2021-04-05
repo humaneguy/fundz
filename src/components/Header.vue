674d4d4e0ba1e99ea2aa6cbba35927d880976313
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-05 09:11:41 +0100

    wip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,25 +4,10 @@
     <header class="h-24 sm:h-32 flex items-center z-30 w-full">
       <div class="container mx-auto px-6 flex items-center justify-between">
         <div class="uppercase text-gray-800 dark:text-white font-black text-3xl">
-          Watch.ME
+          Fundz.ME
         </div>
         <div class="flex items-center">
           <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
-            <a href="#" class="py-2 px-6 flex">
-              Home
-            </a>
-            <a href="#" class="py-2 px-6 flex">
-              Watch
-            </a>
-            <a href="#" class="py-2 px-6 flex">
-              Product
-            </a>
-            <a href="#" class="py-2 px-6 flex">
-              Contact
-            </a>
-            <a href="#" class="py-2 px-6 flex">
-              Carrer
-            </a>
           </nav>
           <button class="lg:hidden flex flex-col ml-4">
                     <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
